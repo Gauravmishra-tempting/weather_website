9198c6a8e074b55584a55029e372c834bc553219
vishwajeettec <vishwajeetsingh@tecraki.io>
2024-11-20 15:37:44 +0530

    add weather website code

--- src/components/pages/weather.jsx
+++ src/components/pages/weather.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 const weather = () => {
   return (
-    <div className="" style={{backgroundColor:"#3A6D8C"}}>
+    <div className="container mt-5 rounded" style={{backgroundColor:"#3A6D8C"}}>
+      <h1 className='text-center p-2'>Weather App</h1>
       <div className="d-flex justify-content-center">
-        <div className="col-12 col-md-6">
-          <h1 className='text-center'>Weather App</h1>
-          <div className="col-12 col-md-6 rounded bg-dark bg-gradient">
-            <h4 className='text-white text-center border-bottom'>Thunder expected around </h4>
+         <div className="row">
+          <div className="col-12 rounded mt-5 bg-dark bg-gradient">
+            <h4 className='text-white text-center'>Thunder expected around</h4>
+            <h1 className='border-bottom'></h1>
+          </div> 
           </div>
         </div>
       </div>
-    </div>
-
   )
 }
 
